523150cdf7b5a92a38cd7c6118041cde7c64ffc8
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-09-17 13:29:06 +0200

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -37,11 +37,15 @@ def validate_data(values):
     of if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+
+    else:
+        pass
 
 get_sales_data()
